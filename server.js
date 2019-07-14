22ef1e373f7f0d37c888380fd7064218141a53e3
Glitch (s-projects18-fcc-53) <none>
2019-07-14 08:49:57 +0000

    basic url check

--- server.js
+++ server.js
@@ -16,22 +16,79 @@ var port = process.env.PORT || 3000;
 
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
+// handle POST-data
 // you should mount the body-parser here
+var bodyParser = require('body-parser');
+// make POST data available in myApp.js, must be placed before all routes
+app.use(bodyParser.urlencoded({extended: false}));
 
+// mount core url module
+// https://nodejs.org/api/url.html
+const url = require('url');
+
+// mount core dns module
+// https://nodejs.org/api/dns.html
+const dns = require('dns');
+
+// make assets public, eg /public/style.css
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// homepage
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+// POST /api/shorturl/new - url
+// chaining and passing variables:
+// - https://davidburgos.blog/how-to-pass-parameters-between-middleware-in-expressjs/
+app.post("/api/shorturl/new", 
+  function(req, res, next) { // check url-syntax
+    let url=false;
+
+    try {
+      url = new URL(req.body.url);
+      //url = new URL('asdhdjask');
+      res.locals.url = url;
+      next();
+    } catch(e) {
+      res.json({"url-syntax-error":req.body.url});  
+      console.log(e);
+    }
+  },
+  function(req, res, next) { // dns-lookup   
+    dns.lookup(res.locals.url.host, (err, address, family) => {
+      if(err) {
+        res.json({"url-dns-error":err, "url":res.locals.host});  
+        console.log(err);
+      } else {
+        //console.log('address: %j family: IPv%s', address, family);
+        next();
+      }
+    }); 
+  },
+  function(req, res) { // all done
+    res.locals.urlNoProtocol = res.locals.url.host + res.locals.url.pathname + res.locals.url.search+ res.locals.url.hash;
+    res.json({"original_url":res.locals.urlNoProtocol,"short_url":1}); 
+  }
+);
+
   
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+  
 
+  
+  // check if url exists ? seems not necessary
+  
+  
+  
+  // check if url exists in db -> use existing short_url
+  
+  // write url in db -> get new short_url
+  
+  // remove protocol
+  //res.json({"original_url":req.body.url,"short_url":1});
 
+// start listening for requests
 app.listen(port, function () {
   console.log('Node.js listening ...');
 });
