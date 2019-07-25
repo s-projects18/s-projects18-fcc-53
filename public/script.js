fac59baad2dcb31f7d3d1cf192f60e339c6a1e26
Glitch (s-projects18-fcc-53) <none>
2019-07-25 04:35:39 +0000

    view

--- public/script.js
+++ public/script.js
@@ -1,13 +1,11 @@
 function submitForm() {
-  postData($('#url_input').val(), processData)
+  var url = $('#url_input').val();
+  if(url.indexOf('http')==-1) {
+    alert("Missing Protocol in url: https:// or http://");
+    return false;
+  }
+  postData(url, processData)
   return false;
-}
-
-function processData(data) {
-  var url = 'https://s-projects18-fcc-53.glitch.me/api/shorturl/'+data.short_url;
-  if(data.short_url==undefined) url = "undefined";
-  $('#shorturl').html(url);
-  $('#shorturl').attr('href', url);
 }
 
 function postData(url, next) {
@@ -15,11 +13,19 @@ function postData(url, next) {
         url: 'https://s-projects18-fcc-53.glitch.me/api/shorturl/new',
         method: "POST",
        data: {url:url},
-        statusCode: {404: function() {$('#result1').append("\rPage not found");}
+        statusCode: {404: function() {alert("\rPage not found");}
       }
    })
    .done(function(data){next(data)})
    .fail(function(jqXHR, textStatus){
       alert('error: ' + textStatus);
    });
+}
+
+// next-function
+function processData(data) {
+  var url = 'https://s-projects18-fcc-53.glitch.me/api/shorturl/'+data.short_url;
+  if(data.short_url==undefined) url = "Cannot evaluate URL";
+  $('#shorturl').html(url);
+  $('#shorturl').attr('href', url);
 }
